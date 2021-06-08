6083aa499d267a837d949c604e3bbf9118875e7a
Nicool3 <skq199101@sina.com>
2021-06-08 23:41:07 +0800

    添加测试窗体。

---
+++ CivilTools/MainClass.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CivilTools
+{
+
+    public class MainClass
+    {
+        [CommandMethod("US")]//CAD启动界面
+        public void UiStart()
+        {
+            TestForm myfrom = new TestForm();
+            //  Autodesk.AutoCAD.ApplicationServices.Application.ShowModalDialog(myfrom); //  模态显示
+
+            Autodesk.AutoCAD.ApplicationServices.Application.ShowModelessDialog(myfrom); //  非模态显示
+
+        }
+    }
+}
